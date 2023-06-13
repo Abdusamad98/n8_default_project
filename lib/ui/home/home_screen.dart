cb02782f958829279ef4f90c0a797c4486fe605b
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-06-13 20:32:56 +0500

    part 4

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -8,10 +8,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ScrollController scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Deafult screen"),),
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: const Text("Deafult screen"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: GridView.builder(
+                  controller: scrollController,
+                  itemCount: 1000,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                        margin: const EdgeInsets.all(10),
+                        color: Colors.white,
+                        child: Center(child: Text("Text $index")));
+                  })),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                scrollController.position.animateTo(
+                  400,
+                  duration: Duration(seconds: 2),
+                  curve: Curves.linear,
+                );
+              });
+            },
+            child: Text("Button"),
+          ),
+        ],
+      ),
     );
   }
 }
